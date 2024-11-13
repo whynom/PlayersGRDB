fa04be6551b3a5ef362977cc98b9e272ea9b3e08
ynom <nomatter@domain.com>
2024-11-13 14:08:12 -0800

    Passes first tests. Creates database and records and saves and reads them.

--- PlayersGRDB/ContentView.swift
+++ PlayersGRDB/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  PlayersGRDB
-//
-//  Created by ynom on 11/12/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- PlayersGRDBTests/PlayersGRDBTests.swift
+++ PlayersGRDBTests/PlayersGRDBTests.swift
@@ -1,36 +1,27 @@
-//
-//  PlayersGRDBTests.swift
-//  PlayersGRDBTests
-//
-//  Created by ynom on 11/12/24.
-//
-
 import Testing
 import GRDB
 @testable import PlayersGRDB
 
 struct PlayersGRDBTests {
-
-    @Test func example() async throws {
-        @Test func insert() throws {
-            // Given an empty database
-            let appDatabase = try makeEmptyTestDatabase()
-            
-            // When we insert a player
-            var insertedPlayer = Player(name: "Arthur", score: 1000)
-            try appDatabase.savePlayer(&insertedPlayer)
-            
-            // Then the inserted player has an id
-            #expect(insertedPlayer.id != nil)
-
-            // Then the inserted player exists in the database
-            let fetchedPlayer = try appDatabase.reader.read(Player.fetchOne)
-            
-            #expect(fetchedPlayer == insertedPlayer)
-            
-        }
+    
+    @Test func insert() throws {
+        // Given an empty database
+        let appDatabase = try makeEmptyTestDatabase()
+        
+        // When we insert a player
+        var insertedPlayer = Player(name: "Arthur", score: 1000)
+        try appDatabase.savePlayer(&insertedPlayer)
+        
+        // Then the inserted player has an id
+        #expect(insertedPlayer.id != nil)
+        
+        // Then the inserted player exists in the database
+        let fetchedPlayer = try appDatabase.reader.read(Player.fetchOne)
+        
+        #expect(fetchedPlayer == insertedPlayer)
+        
     }
-
+    
     /// Return an empty, in-memory, `AppDatabase`.
     private func makeEmptyTestDatabase() throws -> AppDatabase {
         let dbQueue = try DatabaseQueue(configuration: AppDatabase.makeConfiguration())
